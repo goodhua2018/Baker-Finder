15d5362d192bbd217931ec9decfcaf2639ef6442
Hua <hualyu.2018@gmail.com>
2022-10-12 21:27:44 +1030

    update database

--- db/db.js
+++ db/db.js
@@ -1,33 +1,33 @@
 const pg = require('pg')
-// const localDbName = 'baker_collective'
+const localDbName = 'baker_collective'
 
-// let db;
-// if (process.env.DATABASE_URL) {
-//   db = new pg.Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-//   })
-// } else {
-//   if (process.env.DB_PASSWORD) {
-//     db = new pg.Pool({
-//       database: localDbName,
+let db;
+if (process.env.DATABASE_URL) {
+  db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  if (process.env.DB_PASSWORD) {
+    db = new pg.Pool({
+      database: localDbName,
 
-//       password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD
 
-//     })
-//   } else {
-//     db = new pg.Pool({
-//       database: localDbName
-//     })
-//   }
-// }
+    })
+  } else {
+    db = new pg.Pool({
+      database: localDbName
+    })
+  }
+}
 
-const db = new pg.Pool({
-    database: 'baker_collective',
-    password: '1111'
-})
+// const db = new pg.Pool({
+//     database: 'baker_collective',
+//     password: '1111'
+// })
 
 
 module.exports = db 
